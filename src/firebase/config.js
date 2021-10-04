67f7c41dc39f85749e7c8dd0f37440d8a822d361
shota nakashima <fantajista5.6nakaji.s@gmail.com>
2021-10-04 14:27:31 +0900

    setting firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0RqiSuZK-P0fmd7NXkS5F20U-VqapbVs",
+  authDomain: "portfolio-midori02.firebaseapp.com",
+  projectId: "portfolio-midori02",
+  storageBucket: "portfolio-midori02.appspot.com",
+  messagingSenderId: "779410540798",
+  appId: "1:779410540798:web:d759cd2c47f13e37f8fdaf",
+  measurementId: "G-X4PDWY2NS8"
+};
