ff54ccc73670b2562d28fe76dc0422ed8b5ced7d
shota nakashima <fantajista5.6nakaji.s@gmail.com>
2021-10-18 14:19:27 +0900

    add to top page content

--- src/components/containers/TopContainser.tsx
+++ src/components/containers/TopContainser.tsx
@@ -1,9 +1,14 @@
 import {FC} from 'react';
+import {useQuery} from 'react-query'
 
 import {TopTemplate} from '../templates'
+import {fetchContents} from '../../lib/contents'
 
 const TopContainer:FC = () => {
-  return<TopTemplate/>
+
+  const userId = 'mTLZenxmFraMwlT5FMjbfPpCCaf2'
+  const contents = useQuery('contents', () =>fetchContents(userId) )
+  return<TopTemplate contents={contents.data}/>
 };
 
 export default TopContainer;
